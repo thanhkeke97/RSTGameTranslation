539b2df25b484cca19b4f69922143e0ba4510ecf
thanhkeke97 <66289800+thanhkeke97@users.noreply.github.com>
2025-05-08 16:56:13 +0700

    update make release

--- MakeReleaseZip.bat
+++ MakeReleaseZip.bat
@@ -1,4 +1,4 @@
-SET APP_NAME=UGTLive
+SET APP_NAME=RSTGameTranslation
 
 rmdir tempbuild /S /Q
 rmdir app\win-x64\publish /S /Q
@@ -12,21 +12,9 @@ copy app\win-x64\publish\*.exe tempbuild
 copy app\chatgpt_config.txt tempbuild
 copy app\gemini_config.txt tempbuild
 copy app\ollama_config.txt tempbuild
-copy media\readme.txt tempbuild
 
 :the server stuff too
 mkdir tempbuild\webserver
 copy app\webserver\*.py tempbuild\webserver
 copy app\webserver\*.bat tempbuild\webserver
-
-call %RT_PROJECTS%\Signing\sign.bat tempbuild\ugtlive.exe "Universal Game Translator Live"
-
-set FNAME=UniversalGameTranslatorLive_Windows.zip
-del %FNAME%
-
-%RT_PROJECTS%\proton\shared\win\utils\7za.exe a -r -tzip %FNAME% tempbuild
-
-:Rename the root folder
-%RT_PROJECTS%\proton\shared\win\utils\7z.exe rn %FNAME% tempbuild\ %APP_NAME%\
-
 pause
