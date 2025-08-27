fec73ff0673432ae6dd4a2b52780a8e6a9aada76
thanhkeke97 <66289800+thanhkeke97@users.noreply.github.com>
2025-08-27 08:30:55 +0700

    Update PaddleOCR and PaddlePaddle installation versions
    Upgraded PaddleOCR to 3.2.0 and adjusted PaddlePaddle and PaddlePaddle-GPU installation commands to use version 3.0.0, including a direct wheel link for GPU support. Removed installation of paddlex and updated related pip commands for improved compatibility.

--- app/webserver/PaddleOCR/SetupServerCondaEnvNVidiaPaddleOCR.bat
+++ app/webserver/PaddleOCR/SetupServerCondaEnvNVidiaPaddleOCR.bat
@@ -24,10 +24,9 @@ call pip install pillow==11.2.1 matplotlib==3.9.4 scipy==1.13.1 tqdm==4.67.1 pyy
 
 REM Install PaddleOCR via pip
 echo Installing PaddleOCR...
-call pip install paddleocr==3.1.1 "paddlex<3.2"
-call pip install paddlepaddle==3.1.1 -i https://www.paddlepaddle.org.cn/packages/stable/cpu/
-call pip install paddlepaddle-gpu==3.1.1 -i https://www.paddlepaddle.org.cn/packages/stable/cu129/
-call pip install paddlex==3.1.3
+call pip install paddleocr==3.2.0
+call pip install paddlepaddle==3.0.0 -i https://www.paddlepaddle.org.cn/packages/stable/cpu/
+call pip install https://paddle-qa.bj.bcebos.com/paddle-pipeline/Develop-TagBuild-Training-Windows-Gpu-Cuda12.9-Cudnn9.9-Trt10.5-Mkl-Avx-VS2019-SelfBuiltPypiUse/86d658f56ebf3a5a7b2b33ace48f22d10680d311/paddlepaddle_gpu-3.0.0.dev20250717-cp39-cp39-win_amd64.whl
 
 
 
